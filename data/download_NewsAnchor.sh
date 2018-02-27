e8baf0f3bc5df00c26c623b9b6b970950936fa33
Haotian <haotianz@stanford.edu>
2018-02-27 05:51:41 +0000

    add newsAnchor

--- data/download_NewsAnchor.sh
+++ data/download_NewsAnchor.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gsutil cp gs://esper/tmp/cloth_dict.pkl .
-gsutil cp gs://esper/tmp/cloth_test.rar .
-tar -xvf cloth_test.rar
+gsutil cp gs://esper/tmp/cloth_test.tar .
+tar -xvf cloth_test.tar
